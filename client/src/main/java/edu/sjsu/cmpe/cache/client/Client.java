75adba0cf3603bdfe411267d4d990821eaa12123
Apoorva Gupta <gupta08.apoorva@gmail.com>
2014-11-11 22:15:02 -0800

    Update Client.java

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -8,43 +8,37 @@ import com.google.common.hash.Hashing;
 public class Client {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        /*CacheServiceInterface cache = new DistributedCacheService("http://localhost:3000");
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
-        System.out.println("Existing Cache Client...");*/
+        
        //adding all servers
         List<DistributedCacheService> Servers = new ArrayList<DistributedCacheService>();
       Servers.add(new DistributedCacheService("http://localhost:3000"));
       Servers.add(new DistributedCacheService("http://localhost:3001"));
       Servers.add(new DistributedCacheService("http://localhost:3002"));
 
-   char[] input ={'0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+   char[] arr ={'0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
    
    
-   //Data put 
-   System.out.println("\nNow we add all the data to the required Cache");
+ 
+   System.out.println("\n Adding data:");
 
-   for(int key=1;key<11;key++)
+   for(int j=1;j<=([arr.lenth]-1);j++)
    {
    
-   int bucket = Hashing.consistentHash(Hashing.md5().hashInt(key), Servers.size());
-   Servers.get(bucket).put(key,Character.toString(input[key]));
+   int x = Hashing.consistentHash(Hashing.md5().hashInt(j), Servers.size());
+   Servers.get(x).put(j,Character.toString(arr[j]));
    //server.get(bucket).put(putkey, value[putkey]);
    
-   System.out.println("PUT --> Key=" + key + " and Value="+ input[key] + " routed to Cache server at localhost://300"+ bucket);
+   System.out.println("PUT --> Key=" + j + " and Value="+ arr[j] + " routed to Cache server at localhost://300"+ x);
    }
 
-   //Get data
-   System.out.println(" ");
+  
    //GET data from Cache servers consistent hashing
-  System.out.println("GET data from Cache... ");
+  System.out.println("\n GET data from Cache... ");
 
-  for(int key=1;key<11;key++)
+  for(int j=1;j<11;j++)
   {
-   int bucket=Hashing.consistentHash(Hashing.md5().hashInt(key), Servers.size());
-       System.out.println("The key value pair " + key +"-" + Servers.get(bucket).get(key)+ " is received to server " + bucket);
+   int x=Hashing.consistentHash(Hashing.md5().hashInt(j), Servers.size());
+       System.out.println("The key value pair " + j +"-" + Servers.get(x).get(j)+ " is received to server " + x);
     }
 }
     
